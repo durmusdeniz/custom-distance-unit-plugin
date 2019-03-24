abd8d7ad0d62cf2f63883fdd9927868d7fdf1b78
Deniz Durmus <denizdurmus87@yandex.com>
2019-03-24 16:42:53 +0800

    finishing up the main handler for customization of the distance units

--- src/main/java/custom/distanceunit/solr/DistanceStrips.java
+++ src/main/java/custom/distanceunit/solr/DistanceStrips.java
@@ -1,10 +1,7 @@
 package custom.distanceunit.solr;
 
 import org.apache.lucene.queries.function.ValueSource;
-import org.apache.lucene.queries.function.valuesource.ConstNumberSource;
-import org.apache.lucene.queries.function.valuesource.DoubleConstValueSource;
-import org.apache.lucene.queries.function.valuesource.MultiValueSource;
-import org.apache.lucene.queries.function.valuesource.VectorValueSource;
+import org.apache.lucene.queries.function.valuesource.*;
 import org.apache.lucene.spatial.SpatialStrategy;
 import org.apache.solr.common.params.SpatialParams;
 import org.apache.solr.schema.AbstractSpatialFieldType;
@@ -27,7 +24,48 @@ public class DistanceStrips extends ValueSourceParser {
 
     @Override
     public ValueSource parse(FunctionQParser functionQParser) throws SyntaxError {
-        return null;
+
+
+        List<ValueSource> sources = functionQParser.parseValueSourceList();
+
+        MultiValueSource mvs1 = parsePoint(functionQParser);
+        MultiValueSource mvs2 = parseSField(functionQParser);
+        double thickness = 5;//Default value
+        double stripAmount = 3;//Default value
+        boolean dynamic = false;
+        String dynamicStrips = null;
+
+        if(sources.size() > 2){
+            throw new SyntaxError("DistanceStrips - Too Many Params: " + sources);
+        }else if(sources.size() == 2){
+            thickness = ((ConstNumberSource)sources.get(0)).getDouble();
+            stripAmount = ((ConstNumberSource)sources.get(1)).getDouble();
+        }else if(sources.size() == 1){
+            if(sources.get(0) instanceof LiteralValueSource){
+                if(((LiteralValueSource)sources.get(0)).getValue().contains(",")){
+                    dynamic = true;
+                    dynamicStrips = ((LiteralValueSource)sources.get(0)).getValue();
+                }else{
+                    throw new SyntaxError("DistanceStrips - Invalid Params: " + sources);
+                }
+            }else if(sources.get(0) instanceof ConstNumberSource){
+                thickness = ((ConstNumberSource) sources.get(0)).getDouble();
+            }else{
+                throw new SyntaxError("DistanceStrips - Invalid Params: " + sources);
+            }
+        }
+
+        double[] constants = getConstants(mvs1);
+
+
+        if(constants != null && mvs2 instanceof VectorValueSource){
+            if(dynamic){
+                return new DistanceStripsHaversineConstFunction(constants[0], constants[1], dynamicStrips, (VectorValueSource) mvs2);
+            }
+            return new DistanceStripsHaversineConstFunction(constants[0], constants[1], thickness, stripAmount, (VectorValueSource)mvs2);
+        }else{
+            throw new SyntaxError("DistanceStrips - Invalid Params: " + sources);
+        }
     }
 
 
